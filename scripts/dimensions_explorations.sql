26d62c2c2cefbe005bd6a0462553e6e9465f05ce
Saddam Hussain <saddam347n@gmail.com>
2025-05-03 07:22:01 +0530

    Update dimensions_explorations.sql

--- scripts/dimensions_explorations.sql
+++ scripts/dimensions_explorations.sql
@@ -1,1 +1,43 @@
+--For Dimension:
 
+--Task: Find the date of the first and last order
+SELECT
+MIN(order_date) first_order_date,
+MAX(order_date) last_order_date
+FROM gold.fact_sales
+
+--How many years of sales are available
+SELECT 
+MIN(order_date) AS first_order_date,
+MAX(order_date) AS last_order_date,
+DATEDIFF(YEAR, MIN(order_date), MAX(order_date)) AS order_range_years
+FROM gold.fact_sales
+
+
+--How many months of sales are available
+SELECT 
+MIN(order_date) AS first_order_date,
+MAX(order_date) AS last_order_date,
+DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold.fact_sales
+
+--How many week of sales are available
+SELECT 
+MIN(order_date) AS first_order_date,
+MAX(order_date) AS last_order_date,
+DATEDIFF(WEEK, MIN(order_date), MAX(order_date)) AS order_range_week
+FROM gold.fact_sales
+
+--Find the youngest and oldest customers
+SELECT 
+MIN(birthdate) AS oldest_birthdate,
+MAX(birthdate) AS youngest_birthdate
+FROM gold.dim_customers
+
+--Find the youngest AGE and oldest AGE customers
+SELECT 
+MIN(birthdate) AS oldest_birthdate,
+DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
+MAX(birthdate) AS youngest_birthdate,
+DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
+FROM gold.dim_customers
