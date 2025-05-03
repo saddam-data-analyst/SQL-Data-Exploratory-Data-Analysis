5600ae20f8e9957e67d56eb682f50fc240221d56
Saddam Hussain <saddam347n@gmail.com>
2025-05-03 07:26:59 +0530

    Update ranking_analysis.sql

--- scripts/ranking_analysis.sql
+++ scripts/ranking_analysis.sql
@@ -1,1 +1,105 @@
+--Ranking (Analysis)
+
+--Which 5 products generate the highest revenue?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC
+
+--Which 5 products(Subcategories) generate the highest revenue?
+SELECT TOP 5
+p.subcategory,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.subcategory
+ORDER BY total_revenue DESC
+
+--using WINDOW FUNCTIONS to get above answer.(It should use for complex query)
+SELECT
+*
+FROM(
+	SELECT
+	p.subcategory,
+	SUM(f.sales_amount) total_revenue,
+	ROW_NUMBER() OVER(ORDER BY SUM(f.sales_amount) DESC) AS rank_sub_categories
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+	GROUP BY p.subcategory
+	)t
+WHERE rank_sub_categories <= 5
+
+--Using WINDOW FUNCTION for Products
+SELECT
+*
+FROM(
+	SELECT
+	p.product_name,
+	SUM(f.sales_amount) total_revenue,
+	ROW_NUMBER() OVER(ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+	GROUP BY p.product_name
+	)t
+WHERE rank_products <=5
+
+
+
+--What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC
+
+--What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+p.subcategory,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.subcategory
+ORDER BY total_revenue ASC
+
+--Find the top 10 customers who have generate the highest revenue
+SELECT TOP 10
+c.customer_key,
+c.first_name,
+c.last_name,
+SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+GROUP BY 
+c.customer_key,
+c.first_name,
+c.last_name
+ORDER BY total_revenue DESC
+
+
+--The 3 customers with the fewest orders placed
+SELECT TOP 3
+c.customer_key,
+c.first_name,
+c.last_name,
+COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+GROUP BY 
+c.customer_key,
+c.first_name,
+c.last_name
+ORDER BY total_orders ASC
 
