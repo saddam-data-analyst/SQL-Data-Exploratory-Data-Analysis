7d904ad58be2cc278583605fe7c601e052780bce
Saddam Hussain <saddam347n@gmail.com>
2025-05-03 07:30:10 +0530

    Update cumulative_analysis.sql

--- scripts/cumulative_analysis.sql
+++ scripts/cumulative_analysis.sql
@@ -1,1 +1,75 @@
+--Advanced Analytics Project (Cumulative Analysis) using WINDOW functions
+--It is aggregate the data progressively over time
 
+
+--Calculate the total sales per month and the running total of sales over time.
+SELECT  --window function
+order_date,
+total_sales,
+SUM(total_sales) OVER(PARTITION BY order_date ORDER BY order_date) AS running_total_sales
+FROM(
+	SELECT
+	DATETRUNC(MONTH, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales
+	FROM gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+)t
+
+--Calculate the total sales per YEAR and the running total of sales over time.
+SELECT  --window function
+order_date,
+total_sales,
+SUM(total_sales) OVER(ORDER BY order_date) AS running_total_sales,
+AVG(avg_price) OVER(ORDER BY order_date) AS moving_avg_price --Moving-Avg
+FROM(
+	SELECT
+	DATETRUNC(YEAR, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	AVG(price) AS avg_price
+	FROM gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(YEAR, order_date)
+)t
+
+
+--Advanced analytics(Performance Analysis) using WINDOW function
+--It is Comparing the current value to a target value.
+
+--Task: Analyze the yearly performance of products by comparing each product's sales to both its average
+--sales performance and the previous year's sales
+
+WITH yearly_product_sales AS (
+	SELECT 
+	YEAR(f.order_date) AS order_year,
+	p.product_name,
+	SUM(f.sales_amount) AS current_sales
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON f.product_key = p.product_key
+	WHERE order_date IS NOT NULL
+	GROUP BY
+	YEAR(f.order_date),
+	p.product_name
+)
+SELECT
+order_year,
+product_name,
+current_sales,
+AVG(current_sales) OVER(PARTITION BY product_name) AS avg_sales,
+current_sales - AVG(current_sales) OVER(PARTITION BY product_name) AS diff_avg,
+CASE
+	WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) > 0 THEN 'Above Avg'
+	WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) < 0 THEN 'Below Avg'
+	ELSE 'Avg'
+END avg_change,
+--Year-Over-Year Analysis
+LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) py_sales,
+current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS diff_py,
+CASE
+	WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+	WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+	ELSE 'No Change'
+END py_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
